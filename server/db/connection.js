629628f86967a38ea6b1f299561a636ca97e3633
anandkhushi05 <136899676+anandkhushi05@users.noreply.github.com>
2025-05-10 11:05:50 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = `mongodb+srv://anandkhushi05:12345@cluster0.hbsptiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.hbsptiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.connect(url, {
     useNewUrlParser: true, 
